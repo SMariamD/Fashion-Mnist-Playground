b4dc5d9f2e24270480f105415d820c8b1463bffe
SMariamD <syedamariamdanish2020@gmail.com>
2025-09-28 18:36:55 +0400

    Add sudoku_logic.py with core function stubs

---
+++ sudoku_logic.py
@@ -0,0 +1,32 @@
+# Sudoku Logic Module
+# This module contains the core logic for solving Sudoku puzzles
+
+def is_valid(board, row, col, num):
+    """
+    Check if placing 'num' at position (row, col) is valid
+    """
+    pass
+
+def solve_sudoku(board):
+    """
+    Solve the Sudoku puzzle using backtracking algorithm
+    """
+    pass
+
+def print_board(board):
+    """
+    Print the Sudoku board in a formatted way
+    """
+    pass
+
+def load_puzzle(filename):
+    """
+    Load a Sudoku puzzle from a text file
+    """
+    pass
+
+def save_solution(board, filename):
+    """
+    Save the solved puzzle to a text file
+    """
+    pass
